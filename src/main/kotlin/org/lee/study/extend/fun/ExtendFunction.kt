80501f91f20a589712d7ec9fd6ce83c37988a021
coward-lee <1398400350@qq.com>
2023-01-01 21:09:05 +0800

    fun and field extend

--- src/main/kotlin/org/lee/study/extend/fun/ExtendFunction.kt
+++ src/main/kotlin/org/lee/study/extend/fun/ExtendFunction.kt
@@ -1,7 +1,25 @@
 package org.lee.study.extend.`fun`
 
 
-
-fun String.println(){
+fun String.println() {
     println(this);
 }
+
+/**
+ * 成员变量
+ */
+var <T> MutableList<T>.firstElement: T
+    get() {
+        return this[0]
+    }
+    set(value) {
+        this[0] = value
+    }
+
+
+
+fun main(args: Array<String>) {
+    val mutableList = mutableListOf("ac", "bs", "cd")
+    println(mutableList.firstElement)
+}
+
